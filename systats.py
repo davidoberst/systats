c2b4d3e9bc4d12d3fe439b2c7c19198288a2ed71
davidoberst <juandavidbustosperez82@gmail.com>
2026-08-27 17:16:51 -0500

    agregar cpu temp y ram

--- systats.py
+++ systats.py
@@ -7,3 +7,26 @@
 #gpu usage
 #consumo de net
 #consumo de disco
+
+import os
+import psutil
+
+cpu = psutil.cpu_percent(interval=1)
+
+temperature = psutil.sensors_temperatures()
+for x, y in temperature.items():
+    for z in y[:len(y)-1]: #solo mostrar temperatura actual
+        temp = z.current
+
+
+ram = psutil.virtual_memory()
+totalRAM = round(ram.total / (1024**3),2)
+
+
+
+
+print(f"CPU : {cpu}")
+print(f"TEMP : {temp}°")
+print(f"RAM : {totalRAM}")
+
+
