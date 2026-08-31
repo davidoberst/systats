6186abab7b1ae18940d77ab2a634e39ded725314
davidoberst <juandavidbustosperez82@gmail.com>
2026-08-31 00:30:15 -0500

    ejecucion en terminal con bug

--- systats.py
+++ systats.py
@@ -12,40 +12,41 @@ import os
 import psutil
 import subprocess
 
+while True : 
+ cpu = psutil.cpu_percent(interval=1)
 
-cpu = psutil.cpu_percent(interval=1)
+ temperature = psutil.sensors_temperatures()
 
-temperature = psutil.sensors_temperatures()
-
-vram = subprocess.run(
+ vram = subprocess.run(
     ["cat", "/sys/class/drm/card1/device/mem_info_vram_used"],
     capture_output=True,
     text=True
-)
+ )
 
-vramtotal = subprocess.run(
+ vramtotal = subprocess.run(
     ["cat","/sys/class/drm/card1/device/mem_info_vram_total"],
     capture_output=True,
     text=True
     
-)
+ )
 
-for x, y in temperature.items():
+ for x, y in temperature.items():
     for z in y[:len(y)-1]: #solo mostrar temperatura actual
         temp = z.current
 
 
-ram = psutil.virtual_memory()
-totalRAM = round(ram.total / (1024**3),2)
-usedRAM = round(ram.used / (1024 **3),2)
-VRAMusage = round(int(vram.stdout) / (1024 **3),2)
-VRAMtotal = round(int(vramtotal.stdout) / (1024 **3),2)
+ ram = psutil.virtual_memory()
+ totalRAM = round(ram.total / (1024**3),2)
+ usedRAM = round(ram.used / (1024 **3),2)
+ VRAMusage = round(int(vram.stdout) / (1024 **3),2)
+ VRAMtotal = round(int(vramtotal.stdout) / (1024 **3),2)
 
-print(f"CPU : {cpu}%")
-print(f"TEMP : {temp}°C")
-print(f"RAM USAGE: {ram.percent}%")
-print(f"RAM : {usedRAM}/{totalRAM}")
-print(f"VRAM : {VRAMusage}GB / {VRAMtotal}GB")
+ print(f"\r CPU : {cpu}%", end="", flush=True)
+ print(f"\rTEMP : {temp}°C", end="", flush=True)
+ print(f"\rRAM USAGE: {ram.percent}%", end="", flush=True)
+ print(f"\rRAM : {usedRAM}/{totalRAM}", end="", flush=True)
+ print(f"VRAM : {VRAMusage}GB / {VRAMtotal}GB", end="", flush=True)
+
 
 
 
