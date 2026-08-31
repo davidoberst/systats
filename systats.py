7e6ea34aa292236b02e894725e1e8f3fd3adb9d7
davidoberst <juandavidbustosperez82@gmail.com>
2026-08-31 00:52:59 -0500

    solucionar bug de repeticion.

--- systats.py
+++ systats.py
@@ -1,54 +1,50 @@
-#cpu
-#temperatira grados centigrados
-#ghz
-#ram usada
-#ram disponible
-#ram reservado y uso de la ram reservada
-#gpu usage
-#consumo de net
-#consumo de disco
+# cpu
+# temperatura grados centigrados
+# ghz
+# ram usada
+# ram disponible
+# ram reservado y uso de la ram reservada
+# gpu usage
+# consumo de net
+# consumo de disco
 
 import os
 import psutil
 import subprocess
 
-while True : 
- cpu = psutil.cpu_percent(interval=1)
+while True:
 
- temperature = psutil.sensors_temperatures()
+    cpu = psutil.cpu_percent(interval=1)
 
- vram = subprocess.run(
-    ["cat", "/sys/class/drm/card1/device/mem_info_vram_used"],
-    capture_output=True,
-    text=True
- )
+    temperature = psutil.sensors_temperatures()
 
- vramtotal = subprocess.run(
-    ["cat","/sys/class/drm/card1/device/mem_info_vram_total"],
-    capture_output=True,
-    text=True
-    
- )
+    vram = subprocess.run(
+        ["cat", "/sys/class/drm/card1/device/mem_info_vram_used"],
+        capture_output=True,
+        text=True
+    )
 
- for x, y in temperature.items():
-    for z in y[:len(y)-1]: #solo mostrar temperatura actual
-        temp = z.current
+    vramtotal = subprocess.run(
+        ["cat", "/sys/class/drm/card1/device/mem_info_vram_total"],
+        capture_output=True,
+        text=True
+    )
 
+    for x, y in temperature.items():
+        for z in y[:len(y)-1]:
+            temp = z.current
 
- ram = psutil.virtual_memory()
- totalRAM = round(ram.total / (1024**3),2)
- usedRAM = round(ram.used / (1024 **3),2)
- VRAMusage = round(int(vram.stdout) / (1024 **3),2)
- VRAMtotal = round(int(vramtotal.stdout) / (1024 **3),2)
+    ram = psutil.virtual_memory()
 
- print(f"\r CPU : {cpu}%", end="", flush=True)
- print(f"\rTEMP : {temp}°C", end="", flush=True)
- print(f"\rRAM USAGE: {ram.percent}%", end="", flush=True)
- print(f"\rRAM : {usedRAM}/{totalRAM}", end="", flush=True)
- print(f"VRAM : {VRAMusage}GB / {VRAMtotal}GB", end="", flush=True)
+    totalRAM = round(ram.total / (1024**3), 2)
+    usedRAM = round(ram.used / (1024**3), 2)
 
+    VRAMusage = round(int(vram.stdout) / (1024**3), 2)
+    VRAMtotal = round(int(vramtotal.stdout) / (1024**3), 2)
 
-
-
-
-
+    print("\033[H\033[J", end="")
+    print(f"CPU       : {cpu}%")
+    print(f"TEMP      : {temp}°C")
+    print(f"RAM       : {usedRAM}/{totalRAM} GB")
+    print(f"RAM USAGE : {ram.percent}%")
+    print(f"VRAM      : {VRAMusage}/{VRAMtotal} GB")
